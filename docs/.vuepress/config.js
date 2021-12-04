2906c4d2e11e4977fdbffd2eb6eac9daf29ebed9
zhoulf <zhoulf@citycloud.com.cn>
2021-12-04 17:23:18 +0800

    feat: 发布

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@
  * @FilePath: /vuepress-font-end/docs/.vuepress/config.js
  * @Date: 2021-12-04 10:27:46
  * @LastEditors: zhoulf
- * @LastEditTime: 2021-12-04 15:21:31
+ * @LastEditTime: 2021-12-04 17:22:51
  * @Description:
  */
 const head = require('./config/head.js')
@@ -11,6 +11,7 @@ const themeConfig = require('./config/themeConfig.js')
 module.exports = {
   // theme: 'vdoing', // 使用npm包主题
   // base:"https://zhoulongf.github.io/vuepress-font-end/",
+  base:"/vuepress-font-end/",
   title: "菲 VuePress",
   description: "Just playing around",
   markdown: {
